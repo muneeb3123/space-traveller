ff70a9f92f83be236e7cf654d471480597d9bf66
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-05-22 16:24:27 -0300

    add linters config

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit
+          {' '}
+          <code>src/App.js</code>
+          {' '}
+          and save to reload.
         </p>
         <a
           className="App-link"
