490a202e03290f483ba25855ed76d6958dca190f
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-05-23 02:47:51 +0500

    Fix errors

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Profile from './routes/Profile';
-import Missions from './routes/Missions';
-import Rockets from './routes/Rockets';
+import Profile from './components/profile/Profile';
+import Missions from './components/Missions/Missions';
+import Rockets from './components/Rocket/Rockets';
 
 function App() {
   return (
-    <Router>
-      <>
+    <>
+      <Router>
         <NavBar />
         <Routes>
-          <Route path="/rockets" component={Rockets} />
-          <Route path="/missions" component={Missions} />
-          <Route path="/profile" component={Profile} />
+          <Route path="/rockets" element={<Rockets />} />
+          <Route path="/missions" element={<Missions />} />
+          <Route path="/profile" element={<Profile />} />
         </Routes>
-      </>
-    </Router>
+      </Router>
+    </>
   );
 }
 
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Profile = () => (
-  <div>
-    <h2>This is my Profile</h2>
-  </div>
+  <>
+    <div>
+      <h2>This is my Profile</h2>
+    </div>
+  </>
 );
 
 export default Profile;
