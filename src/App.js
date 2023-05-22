6c302f6a3374426212bc4b457210d2dd27e86410
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-05-22 18:29:45 -0300

    add NavBar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Profile from './routes/Profile';
+import Missions from './routes/Missions';
+import Rockets from './routes/Rockets';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <NavBar />
+        <Routes>
+          <Route path="/rockets" component={Rockets} />
+          <Route path="/missions" component={Missions} />
+          <Route path="/profile" component={Profile} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
