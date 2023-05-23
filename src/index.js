5e75eda4b6ba8a77e1c2963f6ec334796accf409
Federica Ulzurrun <fzulzurrun@gmail.com>
2023-05-23 11:43:25 -0300

    fecth data from spacexdata

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom/client';
+import store from './Redux/store';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
