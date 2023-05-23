e92c09ba80100b2aa9f1c301b4cf21de34523387
muneeb3123 <rehmanmuneeb66@gmail.com>
2023-05-24 00:55:21 +0500

    render rocket component from api

--- src/components/Rocket/Rockets.js
+++ src/components/Rocket/Rockets.js
@@ -1,16 +1,27 @@
 import { useEffect, React } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../../Redux/Rockets/RocketSlice';
+import './Rocket.css';
 
 function Rockets() {
   const dispatch = useDispatch();
-  const { status } = useSelector((state) => state.rockets);
+  const { data, status } = useSelector((state) => state.rockets);
   useEffect(() => {
     dispatch(fetchData());
   }, [dispatch]);
   return (
     <div className="rocket-container">
       {status && <div>Loading...</div>}
+      {data && data.map((rocket) => (
+        <div key={rocket.id} className="main-container">
+          <img className="rocket-img" src={rocket.flickr_images} alt={rocket.rocket_name} />
+          <div className="rocket-content">
+            <h3 className="header">{rocket.rocket_name}</h3>
+            <p className="description">{rocket.description}</p>
+            <button type="submit" className="reserved">Reserve Rocket</button>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
