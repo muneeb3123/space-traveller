d07ddce400d905c0e97a2ce9e3f54b2f1387ca61
muneeb3123 <rehmanmuneeb66@gmail.com>
2024-02-15 19:49:53 +0500

    remove strict mode

--- src/index.js
+++ src/index.js
@@ -8,9 +8,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
       <App />
-    </React.StrictMode>
   </Provider>,
 );
 
